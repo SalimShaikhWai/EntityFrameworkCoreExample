35f58e15d173dbeab8fbc144861b1f4639bbe4cd
Salim Shaikh <salim.shaikh@waiin.com>
2022-06-16 16:11:27 +0530

    Add project files.

---
+++ EntityFrameworkCoreExample/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreExample.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual  DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=WAIANGDESK21\MSSQLSERVER01;Initial Catalog=StudentMgt;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
